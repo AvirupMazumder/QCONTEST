90e44eb136376bb8a52640c6db5e3091a1b8a656
CrioUser <crio-user@criodo.com>
2024-12-04 06:54:44 +0530

    ME_QCONTEST_MODULE_ENTITIES

--- java/src/main/java/com/crio/qcontest/entities/User.java
+++ java/src/main/java/com/crio/qcontest/entities/User.java
@@ -34,6 +34,11 @@ public class User {
     // 2) Throw a Runtime Exception with an appropriate message for invalid score.
 
     public void modifyScore(Integer score){
+        if(score < 0) {
+            throw new RuntimeException("User cannot have negative score");
+        } else {
+            totalScore = score;
+        }
     }
 
     @Override
